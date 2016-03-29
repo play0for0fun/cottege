d45dd352371f1e102fea21fae15ea8fee76aeece
webdan-man <i.const.yank@gmail.com>
2016-03-29 08:39:44 +0300

    правки 26.03

--- js/map.js
+++ js/map.js
@@ -6,11 +6,11 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 11,
+        zoom: 9,
         scrollwheel: false,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(55.73109154702577,37.77617385277861), // New York
+        center: new google.maps.LatLng(55, 37.8), // New York
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -23,13 +23,42 @@ function init() {
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
+    
 
     // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
+
+   var waypts = [];
+      directionsService = new google.maps.DirectionsService();
+      directionsDisplay = new google.maps.DirectionsRenderer({
+      });
+      
+          var request = {
+              origin: new google.maps.LatLng(55.755826, 37.6173),
+              destination: new google.maps.LatLng(55.9814542, 36.79829100000006),
+              waypoints: waypts,
+              travelMode: google.maps.DirectionsTravelMode.DRIVING
+          };
+
+          directionsService.route(request, function(response, status) {
+              if (status == google.maps.DirectionsStatus.OK) {
+                  polylineOptions = {
+                      strokeColor: '#5a9fff',
+                      strokeWeight: '3'
+                  }
+                  directionsDisplay.setOptions({
+                      polylineOptions: polylineOptions
+                  });
+                  directionsDisplay.setDirections(response);
+              }
+          });
+          directionsDisplay.setMap(map);
+
+
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(55.73109154702577,37.62617385277861),
+        position: new google.maps.LatLng(56.04573,36.734034),
         map: map,
         icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
   origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
@@ -37,7 +66,7 @@ function init() {
     });
 
 
-     var contentString = 'Название поселка';
+     var contentString = 'КП "Куртниково"';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString
@@ -48,15 +77,15 @@ function init() {
   });
 
   var marker2 = new google.maps.Marker({
-        position: new google.maps.LatLng(55.73129154702577,37.64617385277861),
+        position: new google.maps.LatLng(56.083255,36.714972),
         map: map,
         icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
-  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(90, 128),},
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
         title: 'Название поселка'
     });
 
 
-     var contentString2 = 'Название поселка2';
+     var contentString2 = 'КП "Лисички"';
 
   var infowindow2 = new google.maps.InfoWindow({
     content: contentString2
@@ -65,4 +94,357 @@ function init() {
   marker2.addListener('click', function() {
     infowindow2.open(map, marker2);
   });
+
+var marker3 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.059385,36.738419),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString3 = 'КП "ЛЕТО"';
+
+  var infowindow3 = new google.maps.InfoWindow({
+    content: contentString3
+  });
+
+  marker3.addListener('click', function() {
+    infowindow3.open(map, marker3);
+  });
+
+var marker4 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.046137,36.677449),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString4 = 'КП "Леоново"';
+
+  var infowindow4 = new google.maps.InfoWindow({
+    content: contentString4
+  });
+
+  marker4.addListener('click', function() {
+    infowindow4.open(map, marker4);
+  });
+
+var marker5 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.08953,36.739442),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString5 = 'КП "Озерный край-2"';
+
+  var infowindow5 = new google.maps.InfoWindow({
+    content: contentString5
+  });
+
+  marker5.addListener('click', function() {
+    infowindow5.open(map, marker5);
+  });
+
+var marker6 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.005294,36.727604),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString6 = 'КП "Zorino Sport Village"';
+
+  var infowindow6 = new google.maps.InfoWindow({
+    content: contentString6
+  });
+
+  marker6.addListener('click', function() {
+    infowindow6.open(map, marker6);
+  });
+
+var marker7 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.004605,36.714936),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString7 = 'КП "Zorino Active Resort"';
+
+  var infowindow7 = new google.maps.InfoWindow({
+    content: contentString7
+  });
+
+  marker7.addListener('click', function() {
+    infowindow7.open(map, marker7);
+  });
+
+var marker8 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.051129,36.700132),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString8 = 'КП "Экопарк "Ушаково"';
+
+  var infowindow8 = new google.maps.InfoWindow({
+    content: contentString8
+  });
+
+  marker8.addListener('click', function() {
+    infowindow8.open(map, marker8);
+  });
+
+var marker9 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.055932,36.686270),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString9 = 'КП "Ушаковские дачи"';
+
+  var infowindow9 = new google.maps.InfoWindow({
+    content: contentString9
+  });
+
+  marker9.addListener('click', function() {
+    infowindow9.open(map, marker9);
+  });
+
+var marker10 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.057534,36.682365),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString10 = 'КП "Ушаковские дачи-2"';
+
+  var infowindow10 = new google.maps.InfoWindow({
+    content: contentString10
+  });
+
+  marker10.addListener('click', function() {
+    infowindow10.open(map, marker10);
+  });
+
+
+var marker11 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.084433,36.755852),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString11 = 'КП "Берег Honka" ';
+
+  var infowindow11 = new google.maps.InfoWindow({
+    content: contentString11
+  });
+
+  marker11.addListener('click', function() {
+    infowindow11.open(map, marker11);
+  });
+
+
+var marker12 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.089880,36.739276),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString12 = 'КП "Озерныйкрай. Лыщево"';
+
+  var infowindow12 = new google.maps.InfoWindow({
+    content: contentString12
+  });
+
+  marker12.addListener('click', function() {
+    infowindow12.open(map, marker12);
+  });
+
+
+var marker13 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.051789,36.738482),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString13 = 'КП "Тихая Заводь"';
+
+  var infowindow13 = new google.maps.InfoWindow({
+    content: contentString13
+  });
+
+  marker13.addListener('click', function() {
+    infowindow13.open(map, marker13);
+  });
+
+
+var marker14 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.089260,36.710995),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString14 = 'КП "Никитское"';
+
+  var infowindow14 = new google.maps.InfoWindow({
+    content: contentString14
+  });
+
+  marker14.addListener('click', function() {
+    infowindow14.open(map, marker14);
+  });
+
+
+var marker15 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.090040,36.743260),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString15 = 'КП "Like-Side"';
+
+  var infowindow15 = new google.maps.InfoWindow({
+    content: contentString15
+  });
+
+  marker15.addListener('click', function() {
+    infowindow15.open(map, marker15);
+  });
+
+
+var marker16 = new google.maps.Marker({
+        position: new google.maps.LatLng(55.982460,36.772408),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString16 = 'КП "Шелестово"';
+
+  var infowindow16 = new google.maps.InfoWindow({
+    content: contentString16
+  });
+
+  marker16.addListener('click', function() {
+    infowindow16.open(map, marker16);
+  });
+
+
+var marker17 = new google.maps.Marker({
+        position: new google.maps.LatLng(55.989493,36.832560),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString17 = 'КП "Дачи Honka" ';
+
+  var infowindow17 = new google.maps.InfoWindow({
+    content: contentString9
+  });
+
+  marker17.addListener('click', function() {
+    infowindow17.open(map, marker17);
+  });
+
+
+var marker18 = new google.maps.Marker({
+        position: new google.maps.LatLng(55.971158,36.827711),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString18 = 'КП "Лесная Пристань"';
+
+  var infowindow18 = new google.maps.InfoWindow({
+    content: contentString18
+  });
+
+  marker18.addListener('click', function() {
+    infowindow18.open(map, marker18);
+  });
+
+
+var marker19 = new google.maps.Marker({
+        position: new google.maps.LatLng(55.973806,36.821531),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString19 = 'КП " Лесная Пристань-2"';
+
+  var infowindow19 = new google.maps.InfoWindow({
+    content: contentString19
+  });
+
+  marker19.addListener('click', function() {
+    infowindow19.open(map, marker19);
+  });
+
+
+var marker20 = new google.maps.Marker({
+        position: new google.maps.LatLng(56.055932,36.686270),
+        map: map,
+        icon:{url:'img/point.png',origin: new google.maps.Point(0, 0),
+  origin: new google.maps.Point(-5, 0),size: new google.maps.Size(35, 33),},
+        title: 'Название поселка'
+    });
+
+
+     var contentString20 = 'КП "РЕКА-РЕКА"';
+
+  var infowindow20 = new google.maps.InfoWindow({
+    content: contentString20
+  });
+
+  marker20.addListener('click', function() {
+    infowindow20.open(map, marker20);
+  });
+   
 }
